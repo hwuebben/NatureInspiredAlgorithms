fcc1cc86616e38ab22a0607f81fb4cfc7120d7f4
arar <aarora@agile-im.de>
2018-05-12 11:30:31 +0200

    Bugfixes.

--- ex3/main.py
+++ ex3/main.py
@@ -14,7 +14,7 @@ evaporator = Evaporator(rho=0.05)
 intensifier = Intensifier(delta=0.05)
 heuristic = TSPHeuristic
 solution_gen = PermutationSolutionGenerator(number_of_ants=50, alpha=1, beta=2, heuristic=heuristic, problem=problem)
-terminator = [maxItTerminator(maxIt=2000), convergenceTerminator(maxIter=50)]
+terminator = [maxItTerminator(maxIt=1), convergenceTerminator(maxIter=50)]
 aco = Ant_Colony_Optimizer(problem, initializer, evaporator, intensifier, solution_gen, terminator, 3, True, True)
 
 solutions, scores = aco.run()
